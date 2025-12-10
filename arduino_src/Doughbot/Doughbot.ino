994b8699dee8caf558a68c74adbfe8e5b2154f64
Gdwade <gdwade@middlebury.edu>
2025-12-09 19:30:34 -0500

    bowl Scanning

--- arduino_src/Doughbot/Doughbot.ino
+++ arduino_src/Doughbot/Doughbot.ino
@@ -6,6 +6,11 @@
 #include "TH_sensor.h"
 #include "TOF.h"
 
+#define MAX_TURNS 36
+#define STEP_MM 3
+// Setp size = 3mm
+
+
 uint8_t rise_fac = 2;
 uint8_t scan_frec = 4;
 uint8_t updating_non_bool = 0;
@@ -19,21 +24,36 @@ uint8_t editing_scan_freq = 0;
 uint8_t state = 2;
 uint8_t prev_state = 2;
 
+uint8_t val_array_len = 0;
+int bowl_min = 0;
+
+double riemannSum();
+double getVolume(int* vals);
+uint32_t scan_bowl();
+uint32_t zero_bowl();
+void unwind_motor();
+
+
 void setup() {
   Serial.begin(115200);
   while (!Serial) {}
   if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
   // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
     Serial.println("Could not find Motor Shield. Check wiring.");
-    while (1);
+    
+  } else {
+    Serial.println("Motor Shield found.");
+    myMotor->setSpeed(30);
+    //init_temp_humid();
   }
-  Serial.println("Motor Shield found.");
-  myMotor->setSpeed(30);
-  init_temp_humid();
-
+ 
+  
   buttons_init();
   lcd_init();
   TOF_init();
+  init_temp_humid();
+  Serial.println("Getting Measurments");
+  getMeasurements();
 }
 
 void loop() {
@@ -132,10 +152,10 @@ void loop() {
       // Home
       display_home_screen();
       if (has_been_pressed(B_DOWN)) {
-        myMotor->step(50, FORWARD, SINGLE);
+        zero_bowl();
       }
       if (has_been_pressed(B_UP)) {
-        myMotor->step(50, BACKWARD, SINGLE);
+        myMotor->step(5, FORWARD, DOUBLE);
       }
       if (has_been_pressed(B_START)) {
         prev_state = state;
@@ -168,5 +188,79 @@ void loop() {
   delay(350);
 }
 
+uint32_t scan_bowl() {
+  uint32_t sum = 0;
+  for (uint8_t i = 0; i < MAX_TURNS; i++ ) {
+    myMotor->step(5, FORWARD, DOUBLE);
+    myMotor->release();
+    int mil = get_mil();
+    if (mil == -1) {
+      return sum; 
+    }
+    if (i == 0) {
+      bowl_min = mil;
+    }
+    //bowl_min - mil
+    sum += (mil) * STEP_MM;
+    delay(250);
+  } 
+  Serial.print("volume (mm^3): "); 
+  Serial.println(sum);
+  return sum;
+}
 
+uint32_t scan_dough() {
+  uint32_t sum = 0;
+  for (uint8_t i = 0; i < MAX_TURNS; i++ ) {
+    myMotor->step(5, FORWARD, DOUBLE);
+    myMotor->release();
+    int mil = get_mil();
+    if (mil == -1) {
+      return sum; 
+    }
+    if (i == 0) {
+      bowl_min = mil;
+    }
+    //bowl_min - mil
+    sum += (bowl_min);
+    delay(250);
+  } 
+  Serial.print("volume (mm^3): "); 
+  Serial.println(sum);
+  return sum;
+}
 
+void unwind_motor() {
+  for (uint8_t i = 0; i < (MAX_TURNS/3); i++ ) {
+    myMotor->step(15, BACKWARD, DOUBLE);
+    delay(50);
+  }
+  myMotor->release();
+}
+
+uint32_t zero_bowl () {
+  uint32_t bowl_slice = scan_bowl();
+  unwind_motor();
+  return bowl_slice;
+}
+
+double riemannSum() {
+  top = -1;
+  //Serial.print("In riemann: ");
+  double sum = 0;
+  for (int i = 0; i < MAX_SIZE; i++) {
+    sum += tof_data[i] * step;  // Riemann Sum of area under the curve
+    //Serial.println(tof_data[i]);
+  }
+  return sum;
+}
+
+double getVolume(int* vals) {
+  double volume = 0;
+  for (int i = 0; i < MAX_SIZE; i++) {
+    volume += i;
+  }
+  volume *= 2 * M_PI * step;
+  volume = volume * (bowl_area - dough_area); // multiply area btwn. the curves
+  return volume;
+}
